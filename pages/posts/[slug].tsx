72d0b9c3faaeaa4b9d8b6bafa26f31e0ba0da59a
SandroMaglione <lass.maglio@gmail.com>
2022-04-24 18:49:19 +0200

    single post page inside post/[slug].tsx

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,73 @@
+import { allPosts, Post } from "contentlayer/generated";
+import {
+  GetStaticPathsResult,
+  GetStaticPropsContext,
+  GetStaticPropsResult,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+// Generate static paths for all posts
+export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+  const paths = allPosts.map((post) => post.url);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// Find post with matching slug and return it as props to the page
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext): Promise<GetStaticPropsResult<{ post: Post }>> {
+  const post = allPosts.find(
+    (post) => post._raw.flattenedPath === params?.slug
+  );
+
+  // Redirect to homepage if post not found
+  return typeof post === "undefined"
+    ? {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      }
+    : {
+        props: {
+          post,
+        },
+      };
+}
+
+const PostLayout: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  post,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+
+      <article>
+        {/* Link back to homepage */}
+        <div>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </div>
+
+        {/* Display parsed markdown content */}
+        <div>
+          <h1>{post.title}</h1>
+          <time dateTime={post.date}>{post.date}</time>
+        </div>
+
+        <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      </article>
+    </>
+  );
+};
+
+export default PostLayout;
