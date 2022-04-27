6463af2e6a7afb4a8e732676f5a492430f80b236
SandroMaglione <lass.maglio@gmail.com>
2022-04-27 04:50:10 +0200

    add all sitemap tags

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,12 +1,23 @@
 import { allPosts } from "contentlayer/generated";
 import type { GetServerSidePropsContext } from "next/types";
 
+// TODO: Change this with your website URL
 const WEBSITE_URL = "https://www.example.com";
+
+// TODO: You could add a custom `priority` and `changefreq` for each page
+// https://www.sitemaps.org/protocol.html
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url><loc>${WEBSITE_URL}</loc></url>
     ${allPosts
-      .map(({ url }) => `<url><loc>${WEBSITE_URL}${url}</loc></url>`)
+      .map(
+        ({ url, date }) => `<url>
+      <loc>${WEBSITE_URL}${url}</loc>
+      <lastmod>${date}</lastmod>
+      <priority>1.00</priority>
+      <changefreq>monthly</changefreq>
+      </url>`
+      )
       .join("\n")}
     </urlset>`;
 
